973aac8166d4451950b34c04c71217f3a5f84527
Steve Park <seongwuk.park@gmail.com>
2020-01-28 16:29:40 +0900

    800

--- src/test/java/edu/bit/board/controller/BoardControllerTest2.java
+++ src/test/java/edu/bit/board/controller/BoardControllerTest2.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import javax.inject.Inject;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,36 +17,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import edu.bit.board.HomeController;
+import edu.bit.board.service.BoardService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(locations={
 		"file:src/main/webapp/WEB-INF/spring/root-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
-public class BoardControllerTest {
+public class BoardControllerTest2 {
 
-	private MockMvc mockMvc;
+	@Inject
+	BoardService boardService;		
 
-	@Before
-	public void setUp(){
-		this.mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
-	}
-		
 	@Test
-	public void test() throws Exception{
-		this.mockMvc.perform(get("/"))
-		.andDo(print())                              // .andDO : �슂泥��뿉 ���븳 泥섎━, print()媛� �씪諛섏쟻
-		.andExpect(status().isOk())                 //.andExpect : �삁�긽媛믪쓣 寃�利앺븳�떎.
-		.andExpect(model().attributeExists("serverTime"))
-		.andReturn();                                //.andReturn : �뀒�뒪�듃 �겢�옒�뒪�뿉�꽌 �옉�꽦�� �븞�뻽吏�留� �뀒�뒪�듃 �븳 寃곌낵 媛앹껜瑜� 諛쏆쓣 �븣 �궗�슜
-
+	public void listTest() throws Exception {
+		System.out.println(boardService.selectAllBoard());
 	}
-
-/*	@Test
-	public void helloTest() throws Exception {
-	 mockMvc.perform(MockMvcRequestBuilders.get("/"))
-	         .andExpect(MockMvcResultMatchers.status().isOk());
-	}*/
 	
 
 
